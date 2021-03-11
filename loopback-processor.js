c136934834b764a414f1cd44781f08d35161fe24
Alex Chronopoulos <achronop@gmail.com>
2021-03-11 09:55:10 +0100

    Simplify and make it stereo

--- loopback-processor.js
+++ loopback-processor.js
@@ -1,24 +1,12 @@
 class Loopback extends AudioWorkletProcessor {
-  constructor() {
-    super();
-    console.info('Constructed loopback');
-  }
-
   process(inputs, outputs, parameters) {
-    if (!inputs) {
-      console.warn('No inputs.');
+    const input = inputs[0];
+    const output = outputs[0];
+
+    for (let channel = 0; channel < output.length; ++channel) {
+      output[channel].set(input[channel]);
     }
 
-    // Mono only.
-    const input = inputs[0][0];
-    const output = outputs[0][0];
-    if (!input) {
-      console.warn('No input; waiting.');
-      return true;
-    }
-    for (let i = 0; i < 128; ++i) {
-      output[i] = input[i];
-    }
     return true;
   }
 }
